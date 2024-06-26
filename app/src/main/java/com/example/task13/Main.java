fd1dac96ea2327abbf1efd032b22a1eff8b9330b
Poki-San <larin44@inbox.ru>
2024-06-26 19:09:37 +0300

    first commit

---
+++ app/src/main/java/com/example/task13/Main.java
@@ -0,0 +1,60 @@
+package com.example.task13;
+
+import java.util.HashSet;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args){
+        HashSet<String> one = new HashSet<>();
+        one.add("gray");
+        one.add("brown");
+        one.add("yellow");
+        one.add("red");
+        one.add("blue");
+
+        HashSet<String> two = new HashSet<>();
+        two.add("red");
+        two.add("green");
+        two.add("brown");
+        two.add("purple");
+        two.add("white");
+        two.add("blue");
+
+        HashSet<Integer> numbers = new HashSet<>();
+        Random rnd = new Random();
+        numbers.add(rnd.nextInt(100)+1);
+        numbers.add(rnd.nextInt(100)+1);
+        numbers.add(rnd.nextInt(100)+1);
+        numbers.add(rnd.nextInt(100)+1);
+        numbers.add(rnd.nextInt(100)+1);
+
+        System.out.println("Задание 1");
+        System.out.println(arrayFromArray(one, two)+"\n");
+
+        System.out.println("Задание 2");
+        for (Object x:numbers.toArray()) {
+            if ((Integer) x > 15 && (Integer) x%2==0) {
+                System.out.println("Правильное число: " + x);
+            } else {
+                System.out.print(((Integer)x)/2 +" - Изначальное число: ");
+                System.out.print(x);
+                System.out.print("\n");
+            }
+        }
+
+    }
+
+    static<T> HashSet<T> arrayFromArray(HashSet<T> one, HashSet<T> two){
+        final HashSet<T> result =new HashSet<>();
+        for (Object x:one.toArray()){
+            for (Object n:two.toArray()) {
+                if (n == x) {
+                    result.add((T) n);
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
